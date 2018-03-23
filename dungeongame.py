c47e1698b0a8db6e8eb541ac46f79eb4b2d3b2e4
TheBigL <lmohamed1@studentmail.nait.ca>
2018-03-23 02:47:39 -0600

    Initalize the project

---
+++ dungeongame.py
@@ -0,0 +1,70 @@
+#Author: Leban Mohamed
+#Project: Dungeon Game
+
+import random
+import sys
+import os
+#pylint: disable=unused-variable
+
+monsterCount = 0
+def setDifficulty():
+    easy = 4
+    normal = 8
+    hard = 12
+    veryHard = 16
+    intense = 20
+    
+    
+
+    while True:
+        print("Select your difficulty")
+        print("Type EASY, NORMAL, HARD, VERY HARD or INTENSE")
+        difficulty = input(">").upper()
+        if difficulty == 'EASY':
+            monsterCount = easy
+            difficulty = easy
+            buildMap(difficulty)
+            break
+
+        elif difficulty == 'NORMAL':
+            difficulty = normal
+            monsterCount = normal
+            buildMap(difficulty)
+
+        elif difficulty == 'HARD':
+            difficulty = hard
+            buildMap(difficulty)
+
+        elif difficulty == 'VERY HARD':
+            difficulty = veryHard
+            buildMap(difficulty)
+
+        elif difficulty == 'INTENSE':
+            difficulty = intense
+            buildMap(difficulty)
+
+        else:
+            print("Not a valid response!")
+            print("Please pick the following difficulties")        
+
+def buildMap(size):
+    xAxis = list(range(0, size))
+    yAxis = list(range(0, size))
+    dungeonMAP = []
+    
+
+    for xPoint in xAxis:
+        for yPoint in yAxis:
+            gridPoint = (xPoint, yPoint)
+            dungeonMAP.append(gridPoint)
+    return dungeonMAP
+
+def exit():
+    sys.exit()
+      
+
+
+
+monsterCount = 0
+print("Welcome to the Dungeon Game")
+setDifficulty()
